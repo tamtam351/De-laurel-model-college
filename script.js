05bd84a478e2070d2e20e397c8e809551f9719e3
tamtam351 <bisoye63@gmail.com>
2024-10-12 16:02:59 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,57 @@
+const counters = document.querySelectorAll(".counters span");
+const container = document.querySelector(".counters");
+
+let activated = false;
+
+window.addEventListener("scroll", () => {
+    if(
+        pageYoffset > container.offsetTop - container.offsetHeight - 200
+        && activated === false
+    ) {
+        counters.forEach(counter => {
+            counter.innerText = 0;
+
+            let count = 0;
+
+
+            function updateCount() {
+
+
+
+                const target = parseInt(counter,dataset,count);
+
+                if(count < target) {
+
+
+                    count++;
+
+                    counter.innerText = count;
+
+                    setTimeout(updateCount, 10);
+                } else {
+
+                    counter.innerText = target;
+                }
+            }
+
+            updateCount();
+
+            activated = true;
+        });
+
+
+
+    } else if(
+        pageYoffset < container.offsetTop - container.offsetHeight-500
+        || pageYoffset === 0
+        && activated === true
+    ){
+
+        counters.forEach(counter => {
+
+
+            counter.innerText = 0;
+        });
+
+        activated = false;
+});
